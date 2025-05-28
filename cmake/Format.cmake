3f67e7451c497a7ac903ac939877120eb1653e22
Connor Sweeney <connor.sweeney1234@gmail.com>
2025-05-28 14:14:53 +0100

    Better linker options definition and formatting semantics

--- cmake/Format.cmake
+++ cmake/Format.cmake
@@ -4,14 +4,14 @@ if(NOT "${IS_SUBDIRECTORY}")
   file(MAKE_DIRECTORY "${FORMAT_MARKER_DIRECTORY}")
   foreach(FILE ${FORMAT_FILES})
     get_filename_component(FILE_NAME "${FILE}" NAME)
-    set(FORMAT_FILE "${FORMAT_MARKER_DIRECTORY}/${FILE_NAME}.format")
+    set(FORMAT_MARKER "${FORMAT_MARKER_DIRECTORY}/${FILE_NAME}.format")
     add_custom_command(
-      OUTPUT "${FORMAT_FILE}"
+      OUTPUT "${FORMAT_MARKER}"
       COMMAND clang-format -i "${FILE}"
-      COMMAND "${CMAKE_COMMAND}" -E touch "${FORMAT_FILE}"
+      COMMAND "${CMAKE_COMMAND}" -E touch "${FORMAT_MARKER}"
       DEPENDS "${FILE}"
     )
-    list(APPEND FORMAT_MARKERS "${FORMAT_FILE}")
+    list(APPEND FORMAT_MARKERS "${FORMAT_MARKER}")
   endforeach()
   add_custom_target("Format" DEPENDS "${FORMAT_MARKERS}")
   list(APPEND DEPENDENCIES "Format")

bc8c6ab6ad64901d6af560d527a591cfcb8cdb5f
Connor Sweeney <connor.sweeney1234@gmail.com>
2025-06-17 11:58:36 +0100

    Better main organization.

--- program/include/main.hpp
+++ program/include/main.hpp
@@ -1,3 +1,8 @@
 #pragma once
 
+#include <filesystem>
+
+void handle_args(int argc, char *argv[]);
+void initialize_playlist(const std::filesystem::path &path);
+int run_loop();
 int main(int argc, char *argv[]);
--- program/source/main.cpp
+++ program/source/main.cpp
@@ -17,40 +17,51 @@
 
 int main(int argc, char *argv[])
 {
+  handle_args(argc, argv);
+  initialize_playlist("C:/Users/conno/Music/Songs");
+  return run_loop();
+}
+
+void handle_args(int argc, char *argv[])
+{
   if (argc > 1)
   {
     for (int i = 1; i < argc; i++) std::cerr << "Unexpected argument: " << argv[i] << std::endl;
     exit(EXIT_FAILURE);
   }
+}
 
-  std::filesystem::path path = "C:/Users/conno/Music/Songs";
+void initialize_playlist(const std::filesystem::path &path)
+{
   if (!std::filesystem::exists(path) || !std::filesystem::is_directory(path))
   {
     std::cerr << "Invalid path: " << path << std::endl;
     exit(EXIT_FAILURE);
   }
 
-  {
-    std::vector<std::filesystem::path> files = {};
-    for (const std::filesystem::directory_entry &entry : std::filesystem::directory_iterator(path))
-      if (entry.is_regular_file() && entry.path().extension() == ".mp3") files.emplace_back(entry.path());
+  std::vector<std::filesystem::path> files = {};
+  for (const std::filesystem::directory_entry &entry : std::filesystem::directory_iterator(path))
+    if (entry.is_regular_file() && entry.path().extension() == ".mp3") files.emplace_back(entry.path());
 
-    tuim::database.execute("CREATE TABLE IF NOT EXISTS " + tuim::Song::table.definition + ";");
-    tuim::database.execute("BEGIN TRANSACTION;");
-    std::for_each(std::execution::par, files.begin(), files.end(),
-                  [&](const std::filesystem::path &file)
-                  {
-                    if (!tuim::database.query<tuim::Song>("SELECT * FROM songs WHERE path = ?;", file.string()).empty())
-                      return;
-                    tuim::FFmpeg::Tags tags = tuim::ffmpeg.get_tags(file.string());
-                    tuim::database.execute("INSERT INTO songs VALUES (?, ?, ?, ?, ?, ?);", file.string(), tags.artist,
-                                           tags.title, 0.0, 0.0, 0);
-                  });
-    tuim::database.execute("COMMIT;");
-  }
+  tuim::database.execute("CREATE TABLE IF NOT EXISTS " + tuim::Song::table.definition + ";");
+  tuim::database.execute("BEGIN TRANSACTION;");
+  std::for_each(std::execution::par, files.begin(), files.end(),
+                [&](const std::filesystem::path &file)
+                {
+                  if (!tuim::database.query<tuim::Song>("SELECT * FROM songs WHERE path = ?;", file.string()).empty())
+                    return;
+                  tuim::FFmpeg::Tags tags = tuim::ffmpeg.get_tags(file.string());
+                  tuim::database.execute("INSERT INTO songs VALUES (?, ?, ?, ?, ?, ?);", file.string(), tags.artist,
+                                         tags.title, 0.0, 0.0, 0);
+                });
+  tuim::database.execute("COMMIT;");
 
   tuim::interface.song_menu.populate(
     tuim::database.query<tuim::Song>("SELECT * FROM songs ORDER BY LOWER(artist) ASC, LOWER(title) ASC"));
+}
+
+int run_loop()
+{
   for (ftxui::Loop loop = tuim::interface.create_loop(); !loop.HasQuitted();)
   {
     std::vector<tuim::Song> target_songs = {};

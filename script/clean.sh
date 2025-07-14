7e994e196afe74c52ceedaa2099238b88fc80242
Connor Sweeney <connor.sweeney1234@gmail.com>
2025-07-15 00:20:33 +0100

    Better clean script.

--- script/clean.sh
+++ script/clean.sh
@@ -9,12 +9,10 @@ DATABASE=0
 if [ $LEVEL == 0 ]; then
   rm -rf build/TerMusic.dir
 elif [ $LEVEL == 1 ]; then
-  rm -rf build/TerMusic.dir
-  rm -rf build/external
+  rm -rf build/TerMusic.dir build/_deps build/cmake build/vcpkg
 elif [ $LEVEL == 2 ]; then
   rm -rf build
 fi
-rm -rf CPM_modules cpm-package-lock.cmake
 
 if [ $DATABASE == 1 ]; then
   rm -rf user/*.db
